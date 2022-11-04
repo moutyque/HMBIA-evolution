1482fdf211d1d5352d58c9a8691898be46f0e14a
Quentin MOUTY <quentin.mouty@corp.ovh.com>
2022-11-04 12:58:19 +0100

    Improve perf:
    Check only header size
    Use thread pool

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ from urllib.parse import urljoin
 from waybackpy import WaybackMachineCDXServerAPI
 import requests as requests
 from bs4 import BeautifulSoup
+import concurrent.futures
+from concurrent.futures import wait, ALL_COMPLETED
+
+files = dict()
 
 
 def download_from_url(url, when=date.today().strftime("%d/%m/%Y")):
-    #TODO: do it in a independent thread
     # If there is no such folder, the script will create one automatically
-    folder_location = fr'.\webscraping\{when}'
-    if not os.path.exists(folder_location): os.mkdir(folder_location)
+    folder_location = fr'./webscraping/{when}'
+    if not os.path.exists(folder_location): os.makedirs(folder_location)
 
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
@@ -23,14 +26,16 @@ def export_tag(folder_location, soup, url, extension, tag, attribute):
     for link in soup.select(f"{tag}[{attribute}$='{extension}']"):
         # Name the pdf files using the last portion of each link which are unique in this case
         filename = os.path.join(folder_location, link[f'{attribute}'].split('/')[-1])
+        join_url = urljoin(url, link[f'{attribute}'])
         try:
-            resp = requests.get(urljoin(url, link[f'{attribute}']))
+            info = requests.head(join_url, allow_redirects=True)
             same_size = False
             if Path(filename).is_file():
                 with open(filename, 'rb') as f:
-                    same_size = len(f.read()) == len(resp.content)
+                    same_size = len(f.read()) == info.headers.get('Content-Length', -1)
             if not same_size:
                 print(f'Writing {filename}')
+                resp = requests.get(join_url)
                 with open(filename, 'wb') as f:
                     f.write(resp.content)
                     print(os.path.getsize(filename))
@@ -50,9 +55,10 @@ def download_current_version(url):
 def download_past_version(url):
     user_agent = "Mozilla/5.0 (Windows NT 5.1; rv:40.0) Gecko/20100101 Firefox/40.0"
     cdx = WaybackMachineCDXServerAPI(url, user_agent, start_timestamp="2020", end_timestamp="2022")
-    for item in cdx.snapshots():
-        print(item.archive_url)
-        download_from_url(item.archive_url, item.datetime_timestamp.strftime("%Y_%m_%d"))
+    futures = [concurrent.futures.ThreadPoolExecutor(max_workers=4).submit(download_from_url, item.archive_url,
+                                                                           item.datetime_timestamp.strftime("%Y_%m_%d"))
+               for item in cdx.snapshots()]
+    wait(futures)  # ALL_COMPLETED is actually the default
 
 
 # Press the green button in the gutter to run the script.
